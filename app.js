5a3e745be7bee93e1737a04a7a227a3d4e934f8f
Toshko Popov <tpopov@axway.com>
2017-01-27 14:49:37 +0200

    [ARRSOF-80] Implement Call endpoints

--- app.js
+++ app.js
@@ -6,12 +6,5 @@ var Arrow = require('arrow'),
 	server = new Arrow();
 
 // TODO: Define a model that you can use when you run the connector locally for testing.
-server.addModel(Arrow.Model.extend('tbray', {
-	fields: {
-		// TODO: Add fields to it.
-		title: {type: String}
-	},
-	connector: 'appc.twilio'
-}));
 
 server.start();
