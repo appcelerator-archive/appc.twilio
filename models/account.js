ff136c91d44c39b342e1dc50a7a276b05df8fc3d
Toshko Popov <tpopov@axway.com>
2017-01-27 16:38:46 +0200

    Added universal FindAll

---
+++ models/account.js
@@ -0,0 +1,17 @@
+var Arrow = require('arrow');
+
+module.exports = Arrow.Model.extend('account', {
+	fields: {
+		sid: { type: String, required: false },
+		owner_account_sid: { type: String, required: false },
+		friendly_name: { type: String },
+		status: { type: String, required: false },
+		date_created: { type: String },
+		date_updated: { type: String },
+		auth_token: { type: String },
+		type: { type: String },
+		uri: { type: String },
+		subresource_uris: { type: Object },	
+	},
+    connector: "appc.twilio"
+});
