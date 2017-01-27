bfd9ac5b2d2dbcf43859b79d4599f7a5d2c233da
Viktor Chobanov <chobanov10100@mail.bg>
2017-01-27 16:54:30 +0200

    Models

---
+++ models/transcriptions.js
@@ -0,0 +1,16 @@
+var Arrow = require('arrow');
+
+module.exports = Arrow.createModel('transcriptions', {
+    fields: {
+        first_page_uri: { type: String },
+        end: { type: Number },
+        previous_page_uri: { type: String },
+        uri: { type: String },
+        page_size: { type: Number },
+        page: { type: Number },
+        transcriptions: { type: Array },
+        next_page_uri: { type: String },
+        start: { type: Number }
+    },
+    connector: "appc.twilio"
+});
