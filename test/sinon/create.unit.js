f0b2e582ce0f4cdbfaa439eca5b3484849d67e4d
Plamen Todorov <ptodorov@axway.com>
2017-02-21 15:15:35 +0200

    style fix

--- test/sinon/create.unit.js
+++ test/sinon/create.unit.js
@@ -6,22 +6,22 @@ const sinon = require('sinon')
 connector.twilioAPI = twilioAPI
 
 test('### Create Call - Error Case ###', function (t) {
-  //DATA STUFF
+  // DATA STUFF
   const Model = server.getModel('call')
-  function cbError(error) {
+  function cbError (error) {
     t.ok(error)
   }
   const cbErrorSpy = sinon.spy(cbError)
 
-  //MOCKING STUFF
+  // MOCKING STUFF
   const twilioAPIStubError = sinon.stub(
     twilioAPI,
     'createCall',
-    //The same parameters as the real function
+    // The same parameters as the real function
     (Model, values, number, callback) => {
-      //This is the body of the mocked function from twilio API
-      
-      //This is the anonymous function inside create.js      
+      // This is the body of the mocked function from twilio API
+
+      // This is the anonymous function inside create.js
       callback(new Error('dsfsdfsfs'))
     }
   )
@@ -38,27 +38,27 @@ test('### Create Call - Error Case ###', function (t) {
 test('### Create Call - Ok Case ###', function (t) {
   connector.twilioAPI = twilioAPI
 
-  //DATA STUFF
+  // DATA STUFF
   const Model = server.getModel('call')
-  function cbOk(error, data) {
+  function cbOk (error, data) {
     t.notOk(error)
-    t.equal(data, "MyDataPrim")
+    t.equal(data, 'MyDataPrim')
   }
   const cbOkSpy = sinon.spy(cbOk)
 
-  //MOCKING STUFF
+  // MOCKING STUFF
   const twilioAPIStubOk = sinon.stub(
     twilioAPI,
     'createCall',
-    //The same parameters as the real function
+    // The same parameters as the real function
     (Model, values, number, callback) => {
-      //This is the body of the mocked function from twilio API
-      
-      //This is the anonymous function inside create.js
-      callback(null, "MyData")
+      // This is the body of the mocked function from twilio API
+
+      // This is the anonymous function inside create.js
+      callback(null, 'MyData')
     }
   )
-  
+
   // EXECUTION STUFF
   createMethod.bind(connector, Model, {}, cbOkSpy)()
   t.ok(twilioAPIStubOk.calledOnce)
